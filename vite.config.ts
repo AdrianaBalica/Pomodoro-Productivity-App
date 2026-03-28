6379ff713fb2fce103289be0d62e270d2c371479
AdrianaBalica <balica.adi1986@gmail.com>
2026-03-28 17:45:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/pomodoro-app/",
+  base: "/Pomodoro-Productivity-App/",
   server: {
     allowedHosts: true,
   },
